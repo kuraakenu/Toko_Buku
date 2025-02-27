7cb9fc9e87ee5991b694c8aa44aaa5c58631b750
kuraakenu <krakenxg23@gmail.com>
2025-02-27 15:49:38 +0700

    rekursif kelar

--- BookStore.cpp
+++ BookStore.cpp
@@ -4,9 +4,10 @@
 using namespace std;
 
 void menu(int a);
+int addBook(int jumlahBuku, int a);
 int showBook(int i, int jumlahBuku);
 
-const int  kapasitasBuku = 100;
+const int kapasitasBuku = 100;
 
 struct buku{
     string namaBuku;
@@ -30,32 +31,19 @@ int main(){
 
         switch(pil){
             case '1':
-            system("cls");
-                if(jumlahBuku < kapasitasBuku){
-                    cout << "Masukkan Nama Buku: ";
-                    cin.ignore();
-                    getline(cin, daftarBuku[jumlahBuku].namaBuku);
-                    cout << "Masukkan Penerbit Buku: ";
-                    getline(cin, daftarBuku[jumlahBuku].penerbitBuku);
-                    cout << "Masukkan Author Buku: ";
-                    getline(cin, daftarBuku[jumlahBuku].authorBuku);
-                    cout << "Masukkan Tahun Terbit: ";
-                    cin >> daftarBuku[jumlahBuku].tahunTerbit;
-                    jumlahBuku++;
-                    system("cls");
-                    cout << "===| Buku Berhasil Ditambahkan! |===\n";
-                    system("pause");
-                    system("cls");
-                }else{
-                    cout << "===| Kapasitas Rak Sudah Penuh! |===\n";
-                    system("pause");
-                    system("cls");
-                }
+            system("cls");    int a;
+            cout << "===| Add Book |===\n";
+            cout << "Masukkan Jumlah Buku yang Ingin Ditambahkan: ";
+            cin >> a;
+            jumlahBuku = addBook(jumlahBuku, a);
+            cout << "===| Buku Berhasil Ditambahkan |===\n";
+            system("pause");
             break;
             case '2':
-                cout << left << setw(5) << "No" << setw(30) << "Judul Buku" << setw(30) << "Penerbit" << setw(30) << "Pengarang" << setw(15) << "Tahun Terbit" << '\n';
+                cout << left << setw(5) << "No" << setw(30) << "Judul Buku" << setw(30) << "Penerbit" << setw(30) << "Pengarang" << setw(30) << "Tahun Terbit" << setw(15) << "Harga" <<  '\n';
                 showBook(0, jumlahBuku);
                 system("pause");
+                
                 system("cls");
             break;
             case '3':
@@ -75,6 +63,29 @@ int main(){
     return 0;
 }
 
+int addBook(int jumlahBuku, int a){
+    if(a == 0 || jumlahBuku >= kapasitasBuku){
+        if(jumlahBuku >= kapasitasBuku){
+            cout << "===| Rak Sudah Penuh |===\n";
+            system("pause");
+        }
+        return jumlahBuku;
+    }
+    cout << "Masukkan Nama Buku: ";
+    cin.ignore();
+    getline(cin, daftarBuku[jumlahBuku].namaBuku);
+    cout << "Masukkan Penerbit Buku: ";
+    getline(cin, daftarBuku[jumlahBuku].penerbitBuku);
+    cout << "Masukkan Author Buku: ";
+    getline(cin, daftarBuku[jumlahBuku].authorBuku);
+    cout << "Masukkan Tahun Terbit: ";
+    cin >> daftarBuku[jumlahBuku].tahunTerbit;
+    cout << "Masukkan Harga Buku: ";
+    cin >> daftarBuku[jumlahBuku].harga;
+    system("cls");
+    return addBook(jumlahBuku + 1, a - 1);
+}
+
 int showBook(int i, int jumlahBuku){
     if (jumlahBuku <= 0) {
         system("cls");
@@ -83,9 +94,9 @@ int showBook(int i, int jumlahBuku){
         cout << "==============================\n";
         return 0;
     }
-    if (i < sizeof(jumlahBuku)/sizeof(int)) {
-        cout << "---------------------------------------------------------------------------------------------------------------\n";
-        cout << left << setw(5) << i + 1 << setw(30) << daftarBuku[i].namaBuku << setw(30) << daftarBuku[i].penerbitBuku << setw(30) << daftarBuku[i].authorBuku << setw(15) << daftarBuku[i].tahunTerbit << '\n';
+    if (i < jumlahBuku) {
+        cout << "----------------------------------------------------------------------------------------------------------------------------------------\n";
+        cout << left << setw(5) << i + 1 << setw(30) << daftarBuku[i].namaBuku << setw(30) << daftarBuku[i].penerbitBuku << setw(30) << daftarBuku[i].authorBuku << setw(30) << daftarBuku[i].tahunTerbit << setw(15) << daftarBuku[i].harga << '\n';
         return showBook(i + 1, jumlahBuku);
     }
 }
